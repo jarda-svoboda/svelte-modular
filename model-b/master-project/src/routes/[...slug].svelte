71e81d998f68243d764452264f1e97152d901806
Jarda Svoboda <jaroslav.svoboda3@dhl.com>
2020-05-29 17:32:04 +0200

    ADD utils

--- model-b/master-project/src/routes/[...slug].svelte
+++ model-b/master-project/src/routes/[...slug].svelte
@@ -1,29 +1,18 @@
 <script context="module">
-  import config from '../modules';
-
-  const filterPath = (route = '', path = '') => {
-    const data = route.split('/');
-    const slug = path.split('/');
-    return route === path || (
-      data.length === slug.length && !data.filter((s, i) => !(s === slug[i] || (slug[i] && s === '*'))).length
-    );
-  }
-
+  import { getComponent } from '../utils/common.utils';
   export async function preload(page, session) {
-    let component, modulePreload, props = {};
     console.log(page);
-    const { path } = page;
     try {
-      const name = Object.keys(config).find((route) => filterPath(route, path));
-      component = config[name].component;
-      modulePreload = config[name].preload;
+      let props = {};
+      const { path } = page;
+      const { component, componentPreload } = getComponent(path);
+      if (componentPreload) {
+        props = await componentPreload(page, session); 
+      }
+      return { component, props };
     } catch (error) {
       this.error(404, error.message);
     }
-    if(modulePreload) {
-      props = await modulePreload(page, session); 
-    }
-    return { component, props };
   }
 </script>
 
